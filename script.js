057a503370b988b53512927b204fff1a4d502305
mido-webdesign <77772019+mido-webdesign@users.noreply.github.com>
2021-02-06 15:26:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 $(function () {
 
+  // PC版ヘッダーを固定
+  $(window).scroll(function() {
+    var headerHeight = $('#header-js').height();
+    var scroll = $(window).scrollTop();
+    if(scroll >= headerHeight) {
+        $('.header-nav').addClass('fixed');
+    } else {
+        $('.header-nav').removeClass('fixed');   
+    };
+    console.log(headerHeight);
+});
+
+  // スマホ版ハンバーガーメニュー
   $('#hamburger-btn-js').click(function() {
     $('body').toggleClass('is-drawerActive');
   });
